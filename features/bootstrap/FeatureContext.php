be2e7193ad9822851d5c5c53832bc094f5e8a62d
slk500 <slawomir.grochowski@gmail.com>
2016-12-02 17:11:19 +0100

    behat continue

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,11 +5,16 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
 
+require_once __DIR__.'/../../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
+
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends MinkContext implements Context
 {
+
+    private $output;
+
     /**
      * Initializes context.
      *
@@ -20,4 +25,57 @@ class FeatureContext extends MinkContext implements Context
     public function __construct()
     {
     }
+
+    /**
+     * @BeforeScenerio
+     */
+    public function moveIntoTestDir()
+    {
+        if(!is_dir('test')) {
+            mkdir('test');
+        }
+    }
+
+    /**
+     * @AfterScenerio
+     */
+    public function moveOutOfTestDir()
+    {
+        chdir('..');
+        if(is_dir('test')) {
+            system('rm -r'.realpath('test'));
+        }
+    }
+
+
+    /**
+     * @Given there is a file named :filename
+     */
+    public function thereIsAFileNamed($filename)
+    {
+        touch($filename);
+    }
+
+    /**
+     * @When I run :command
+     */
+    public function iRun($command)
+    {
+        $this->output = shell_exec($command);
+    }
+
+    /**
+     * @Then I should see :arg1 in the output
+     */
+    public function iShouldSeeInTheOutput($string)
+    {
+
+        assertContains(
+            $string,
+            $this->output,
+            sprintf('Did not see "%s" in the output "%s"', $string, $this->output)
+        );
+    }
+
+
 }
