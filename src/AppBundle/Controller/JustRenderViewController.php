190723c492e4a267f7e0a3276992f25d64de0ba3
slk500 <slawomir.grochowski@gmail.com>
2017-01-18 00:32:11 +0100

    remove routing.yml, trainingTime

---
+++ src/AppBundle/Controller/JustRenderViewController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slk500
+ * Date: 18.01.17
+ * Time: 00:17
+ */
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class JustRenderViewController extends Controller
+{
+    /**
+     * @Route("/kontakt", name="contact")
+     */
+    public function contactController()
+    {
+        return $this->render('contact/contact.html.twig');
+    }
+
+    /**
+     * @Route("/regulamin", name="rules")
+     */
+    public function rulesController()
+    {
+        return $this->render('rules/rules.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="admin_index")
+     */
+    public function adminController()
+    {
+        return $this->render('admin/index.twig');
+    }
+}
+
