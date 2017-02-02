4e36060e3f191b492fb62d65f7113a1df64910f6
slk500 <slawomir.grochowski@gmail.com>
2017-02-02 20:49:01 +0100

    still dosen't work

--- src/AppBundle/Security/FacebookAuthenticator.php
+++ src/AppBundle/Security/FacebookAuthenticator.php
@@ -10,6 +10,7 @@ namespace AppBundle\Security;
 
 use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,14 +31,18 @@ class FacebookAuthenticator extends SocialAuthenticator
     private $clientRegistry;
     private $em;
     private $router;
+    private $container;
 
-    public function __construct(ClientRegistry $clientRegistry, EntityManager $em, RouterInterface $router)
+    public function __construct(ClientRegistry $clientRegistry,
+        ContainerInterface $container, EntityManager $em, RouterInterface $router)
     {
         $this->clientRegistry = $clientRegistry;
         $this->em = $em;
         $this->router = $router;
+        $this->container = $container;
 
     }
+
 
     public function getCredentials(Request $request)
     {
@@ -53,59 +58,68 @@ class FacebookAuthenticator extends SocialAuthenticator
     {
         /** @var FacebookUser $facebookUser */
         $facebookUser = $this->getFacebookClient()->fetchUserFromToken($credentials);
-        $email = $facebookUser->getEmail();
 
-             $user = $this->em->getRepository('AppBundle:User')
-            ->findOneBy(array('email' => $email));
+        $user = $this->em->getRepository('AppBundle:User')
+                    ->findOneBy(['facebook_id' => $facebookUser->getId()]);
 
-        if(!$user) {
+//        if(!$user) {
+//
+//            // takes URL of image and Path for the image as parameter
+//            function download_image1($image_url, $image_file){
+//                $fp = fopen ($image_file, 'w+');              // open file handle
+//
+//                $ch = curl_init($image_url);
+//                // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // enable if you want
+//                curl_setopt($ch, CURLOPT_FILE, $fp);          // output to file
+//                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+//                curl_setopt($ch, CURLOPT_TIMEOUT, 1000);      // some large value to allow curl to run for a long time
+//                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
+//                // curl_setopt($ch, CURLOPT_VERBOSE, true);   // Enable this line to see debug prints
+//                curl_exec($ch);
+//
+//                curl_close($ch);                              // closing curl handle
+//                fclose($fp);                                  // closing file handle
+//            }
+//
+//            $url = $facebookUser->getPictureUrl();
+//            $file_name = 'fb_temp';
+//
+//            download_image1($url,$file_name);
+//
+//            $file = new File($file_name,true);
+//            $ext = $file->getExtension();
+//
+//            $image_file = new UploadedFile($file_name.$ext, $file_name.$ext, null, null, null, true);
+//
+//            $user = new User();
+//            //$user->setEmail($email);
+////            $user->setName($facebookUser->getFirstName());
+////            $user->setSurname($facebookUser->getLastName());
+////            $male = $facebookUser->getGender() == 'male' ? true : false;
+////            $user->setMale($male);
+////            $user->setImageFile($image_file);
+////            $user->setFacebookId($facebookUser->getId());
+////            $this->em->persist($user);
+////            $this->em->flush();
+//
+//            return new RedirectResponse($this->router->generate('login'));
+//
+//        }else{
+//            $existingUser = $this->em->getRepository('AppBundle:User')
+//                ->findOneBy(['facebook_id' => $facebookUser->getId()]);
+//
+//            if ($existingUser) {
+//                return $existingUser;
+//            }
+//        }
 
-            // takes URL of image and Path for the image as parameter
-            function download_image1($image_url, $image_file){
-                $fp = fopen ($image_file, 'w+');              // open file handle
+        if(!$user){
+            $session = $this->container->get('request')->getRequest()->getSession();
+            $session->set('data' => $_POST);
 
-                $ch = curl_init($image_url);
-                // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // enable if you want
-                curl_setopt($ch, CURLOPT_FILE, $fp);          // output to file
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-                curl_setopt($ch, CURLOPT_TIMEOUT, 1000);      // some large value to allow curl to run for a long time
-                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
-                // curl_setopt($ch, CURLOPT_VERBOSE, true);   // Enable this line to see debug prints
-                curl_exec($ch);
-
-                curl_close($ch);                              // closing curl handle
-                fclose($fp);                                  // closing file handle
-            }
-
-            $url = $facebookUser->getPictureUrl();
-            $file_name = 'fb_temp';
-
-            download_image1($url,$file_name);
-
-            $file = new File($file_name,true);
-            $ext = $file->getExtension();
-
-            $image_file = new UploadedFile($file_name.$ext, $file_name.$ext, null, null, null, true);
-
-            $user = new User();
-            $user->setEmail($email);
-            $user->setName($facebookUser->getFirstName());
-            $user->setSurname($facebookUser->getLastName());
-            $male = $facebookUser->getGender() == 'male' ? true : false;
-            $user->setMale($male);
-            $user->setImageFile($image_file);
-            $user->setFacebookId($facebookUser->getId());
-            $this->em->persist($user);
-            $this->em->flush();
-
-        }else{
-            $existingUser = $this->em->getRepository('AppBundle:User')
-                ->findOneBy(array('facebook_id' => $facebookUser->getId()));
-
-            if ($existingUser) {
-                return $existingUser;
-            }
+            throw new AuthenticationException();
         }
+
 
         return $user;
 
@@ -123,7 +137,11 @@ class FacebookAuthenticator extends SocialAuthenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        return new RedirectResponse($this->router->generate('login'));
+
+        return new RedirectResponse($this->container->get('router')
+            ->generate('homepage'));
+
+
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
