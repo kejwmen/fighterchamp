ce4e04a2c5d058a4e3608224ad898256ba2df34f
slk500 <slawomir.grochowski@gmail.com>
2017-10-01 07:14:51 -0400

    remove signup from fight

--- src/AppBundle/Entity/Fight.php
+++ src/AppBundle/Entity/Fight.php
@@ -251,20 +251,4 @@ class Fight
     {
         $this->youtubeId = $youtubeId;
     }
-
-    /**
-     * @return ArrayCollection|SignUpTournament[]
-     */
-    public function getSignuptournament()
-    {
-        return $this->signuptournament;
-    }
-
-
-    /**
-     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\SignUpTournament", inversedBy="fights")
-     * @ORM\JoinTable(name="signuptournament_fight")
-     * */
-    private $signuptournament;
-
 }
