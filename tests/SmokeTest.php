81753c42ccbab40e589d157318c1afe8f95194bc
slk <slawomir.grochowski@gmail.com>
2018-05-24 01:04:31 +0200

    add smokeTest

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -9,6 +9,7 @@
 namespace Tests;
 
 
+use Shopsys\HttpSmokeTesting\Auth\BasicHttpAuth;
 use Shopsys\HttpSmokeTesting\HttpSmokeTestCase;
 use Shopsys\HttpSmokeTesting\RouteConfig;
 use Shopsys\HttpSmokeTesting\RouteConfigCustomizer;
@@ -19,9 +20,6 @@ class SmokeTest extends HttpSmokeTestCase
 
     protected function customizeRouteConfigs(RouteConfigCustomizer $routeConfigCustomizer)
     {
-
-
-
         $routeConfigCustomizer
             ->customize(function (RouteConfig $config, RouteInfo $info) {
 
@@ -35,7 +33,6 @@ class SmokeTest extends HttpSmokeTestCase
                     'admin_mail',
                     'news',
                     'news_new',
-                    'admin_user_list',
                     'admin_user_new',
                     'connect_facebook',
                     'connect_facebook_check',
@@ -43,13 +40,37 @@ class SmokeTest extends HttpSmokeTestCase
                     'user_edit_view',
                 ];
 
+                $postRoute = [
+                  'user_create',
+                    'api_user_update',
+                    'setNullOnImage',
+                    'api_image_upload',
+                    'admin_user_list'
+                ];
+
                 // This function will be called on every RouteConfig provided by RouterAdapter
                 if ($info->getRouteName()[0] === '_') {
                     // You can use RouteConfig to change expected behavior or skip testing particular routes
                     $config->skipRoute('Route name is prefixed with "_" meaning internal route.');
                 }
+
+
                 if($info->getRouteParameterNames()) $config->skipRoute();
+
+
                 if(in_array($info->getRouteName(), $redirectsRoute)) $config->skipRoute();
+                if(in_array($info->getRouteName(), $postRoute)) $config->skipRoute();
+
+                if ($info->getRouteName()[0] === 'admin') {
+
+                    $config->changeDefaultRequestDataSet('Log in as "user".')
+                        ->setAuth(new BasicHttpAuth('admin', 'Cortez1634'));
+
+                }
+
+                if (!$info->isHttpMethodAllowed('GET')) {
+                    $config->skipRoute('Only routes supporting GET method are tested.');
+                }
 
 
             });
