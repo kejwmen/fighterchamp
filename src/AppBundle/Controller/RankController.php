02fbe036e78bf867dedacc40bf2a4a5d8dc30046
slk <slawomir.grochowski@gmail.com>
2018-12-02 13:40:05 +0100

    add filter to ranks

--- src/AppBundle/Controller/RankController.php
+++ src/AppBundle/Controller/RankController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use AppBundle\Repository\RankRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class RankController extends Controller
 {
     /**
-     * @Route("ranking", name="rank")
+     * @Route("ranking/{year}/{age}", name="rank")
      */
-    public function indexAction(EntityManagerInterface $entityManger)
+    public function indexAction(RankRepository $rankRepository,
+                                string $year = 'wszystkie-lata',
+                                string $age = 'wszystkie-kategorie-wiekowe')
     {
-        $users = $entityManger->getRepository(User::class)->findAllFightersRank();
+
+      $params = [
+        'year' => $year,
+        'age' => $age
+      ];
+
+        $users = $rankRepository->findAllByYearAndAge($params);
 
         return $this->render('rank/rank.html.twig', [
-            'users' => $users
+            'users' => $users,
+            'year' => $year,
+            'age' => $age
         ]);
     }
 }
